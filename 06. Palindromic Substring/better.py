6fde2b0255943e444b06a0e575ca617229a129ae
--global <--global>
2022-02-18 15:42:00 +0900

    wow..

--- 06. Palindromic Substring/better.py
+++ 06. Palindromic Substring/better.py
@@ -1,3 +1,18 @@
 class Solution:
-  def longestPalindrome(self, s: str) -> int:
-    
+    def longestPalindrome(self, s: str) -> int:
+
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # why left +1, right, not left, right + 1? -> moved one more than normal
+            return s[left + 1:right]
+
+        # Need to know - exceptions
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        result = ''
+        # save only the longest pan, instead of saving all of pans
+        for i in range(len(s) - 1):
+          result = max(result, expand(i, i+1), expand(i,i+2), key=len)
+        return result
