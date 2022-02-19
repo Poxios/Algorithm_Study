3a8dabe6be4df25234794f6ad62d300a0cc01fc5
Poxios <poxios0310@gmail.com>
2022-02-19 14:26:27 +0900

    two pointer edited

--- 08. Trapping rain water/better.py
+++ 08. Trapping rain water/better.py
@@ -13,11 +13,13 @@ class Solution:
         left_max, right_max = height[left], height[right]
         # When semi-max pole detected, pull the pole of other side
         while left < right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            
             if left_max <= right_max:
               volume += left_max - height[left]
               left += 1
+              left_max=max(height[left], left_max)
             else:
-              volume += left_max -height[right]
+              volume += right_max -height[right]
               right -= 1
+              right_max = max(height[right], right_max)
         return volume
