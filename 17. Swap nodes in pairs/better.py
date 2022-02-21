8ffb39e5cb00a28260617805d564876c5d74babf
Poxios <poxios0310@gmail.com>
2022-02-21 20:06:30 +0900

    with recursion

---
+++ 17. Swap nodes in pairs/better.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+            p = head.next
+            head.next = self.swapPairs(p.next)
+            p.next = head
+            return p
+        return head
