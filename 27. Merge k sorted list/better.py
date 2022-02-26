40017b27db453f1cee7394ddbd7880056a16d35f
Poxios <poxios0310@gmail.com>
2022-02-26 19:26:31 +0900

    done

--- 27. Merge k sorted list/better.py
+++ 27. Merge k sorted list/better.py
@@ -1,8 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+import heapq
+from typing import Optional, List
+
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
+        root = result = ListNode(None)
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-while None:
-    print('')
+import heapq
 
-    
+def heap_sort(nums):
+  heap = []
+  for num in nums:
+    heapq.heappush(heap, num)
+
+  sorted_nums = []
+  while nums:
+    sorted_nums.append(heapq.heappop(nums))
+  return sorted_nums
+
+print(heap_sort([4, 1, 7, 3, 8, 5]))
