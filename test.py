430534cba32bf78ff877f76357352a79296d7b26
Poxios <poxios0310@gmail.com>
2022-02-27 14:44:51 +0900

    done

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-import heapq
-
-def heap_sort(nums):
-  heap = []
-  for num in nums:
-    heapq.heappush(heap, num)
-
-  sorted_nums = []
-  while nums:
-    sorted_nums.append(heapq.heappop(nums))
-  return sorted_nums
-
-print(heap_sort([4, 1, 7, 3, 8, 5]))
+for i in range(0):
+  print('32')
