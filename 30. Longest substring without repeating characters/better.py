182d5e347224be360c1a397d9a30e70c9863fba8
Poxios <poxios0310@gmail.com>
2022-02-27 15:08:36 +0900

    updated

---
+++ 30. Longest substring without repeating characters/better.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used ={}
+        # need to know - two pointer doesnt need to move together. 
+        max_length = start = 0
+        for index, char in enumerate(s):
+            if char in used and start <= used[char]:
+                start = used[char]+1
+            else:
+                max_length = max(max_length, index - start + 1)
+            used[char] = index
+        return max_length
