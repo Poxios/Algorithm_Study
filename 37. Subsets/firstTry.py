9bea408f884b8209c3739e697161f4c97b875649
Poxios <poxios0310@gmail.com>
2022-03-01 10:23:47 +0900

    firstTry success

---
+++ 37. Subsets/firstTry.py
@@ -0,0 +1,22 @@
+import enum
+import itertools
+from typing import List
+
+# 1,2,3
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results = []
+
+        def dfs(candidates: list, basket: list):
+            results.append(basket)
+            
+            for idx, num in enumerate(candidates):
+                dfs(candidates[idx + 1:], basket + [  num])
+
+        dfs(nums, [])
+        return results
+
+s=Solution()
+s.subsets([1,2,3])
