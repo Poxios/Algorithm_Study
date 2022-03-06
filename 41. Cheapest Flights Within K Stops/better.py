4aaa5841f4a39073c8e77e006b4a2187f19d54d5
Poxios <poxios0310@gmail.com>
2022-03-06 18:57:27 +0900

    updated

--- 41. Cheapest Flights Within K Stops/better.py
+++ 41. Cheapest Flights Within K Stops/better.py
@@ -4,6 +4,7 @@ import collections
 import sys
 from typing import List
 
+# https://github.com/onlybooks/algorithm-interview/issues/104
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
@@ -12,7 +13,6 @@ class Solution:
         # 그래프 인접 리스트 구성
         for u, v, w in flights:
             graph[u].append((v, w))
-
         # 큐 변수: [(가격, 정점, 남은 가능 경유지 수)]
         Q = [(0, src, K)]
 
